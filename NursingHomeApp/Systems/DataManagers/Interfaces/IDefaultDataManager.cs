d1c8a4f99b4e34923f75eff135a24686c868a28c
Szado12 <51293021+Szado12@users.noreply.github.com>
2021-01-04 20:21:33 +0100

    Creating Entities
    Creating Entities

---
+++ NursingHomeApp/Systems/DataManagers/Interfaces/IDefaultDataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NursingHomeApp.Systems.DataMangers.Interfaces
+{
+    interface IDefaultDataManager
+    {
+        bool Add(Object obj);
+        bool Delete(int id);
+        bool Update(Object obj);
+        List<Object> Select();
+        List<Object> Select(int Id);
+    }
+}
