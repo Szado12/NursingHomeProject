ad551139a7eeaacce2562801adbed70a4e56b846
halszka99 <halszka99@interia.eu>
2021-01-05 17:21:02 +0100

    AdministartorForm added
    AdministartorForm added

--- NursingHomeApp/Forms/AdministratorForm.Designer.cs
+++ NursingHomeApp/Forms/AdministratorForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             this.tabControlRehabilitator = new System.Windows.Forms.TabControl();
             this.tabPageInformations = new System.Windows.Forms.TabPage();
             this.labelProfession = new System.Windows.Forms.Label();
@@ -656,5 +657,14 @@
         private System.Windows.Forms.DataGridView dataGridViewPatientsList;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView dataGridViewRooms;
+=======
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "AdministratorForm";
+        }
+
+        #endregion
+>>>>>>> Stashed changes
     }
 }
