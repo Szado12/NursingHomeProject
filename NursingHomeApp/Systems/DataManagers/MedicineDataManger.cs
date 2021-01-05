1e328af3ab9e0a04a367b80538b9b7de5c43c463
Szado12 <51293021+Szado12@users.noreply.github.com>
2021-01-05 15:45:43 +0100

    Creating interface MedicineDataManager

---
+++ NursingHomeApp/Systems/DataManagers/MedicineDataManger.cs
@@ -0,0 +1,37 @@
+﻿using NursingHomeApp.Systems.DataManagers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NursingHomeApp.Systems.DataManagers
+{
+    class MedicineDataManger : DefaultDataManager, IMedicineDataManger
+    {
+        public bool Add(Medicine t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Medicine> Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Medicine Select(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Medicine t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
