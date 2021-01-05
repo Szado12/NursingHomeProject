1baa49a40f330a049c8a55a3a47ff7b5d268effc
Szado12 <51293021+Szado12@users.noreply.github.com>
2021-01-05 17:13:39 +0100

    Rehabilitant form treatment done

---
+++ NursingHomeApp/Systems/DataManagers/TreatmentDataManager.cs
@@ -0,0 +1,42 @@
+﻿using NursingHomeApp.Systems.DataManagers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NursingHomeApp.Systems.DataManagers
+{
+    class TreatmentDataManager : DefaultDataManager, ITreatmentDataManger
+    {
+        public bool Add(Treatment t)
+        {
+            DbContext.Treatments.Add(t);
+            return (DbContext.SaveChanges() > 0);
+        }
+
+        public bool Delete(int Id)
+        {
+            Treatment treatment = DbContext.Treatments.SingleOrDefault(t => t.Id == Id);
+            DbContext.Treatments.Remove(treatment);
+            return (DbContext.SaveChanges() > 0);
+        }
+
+        public List<Treatment> Select()
+        {
+            return DbContext.Treatments.ToList();
+        }
+
+        public Treatment Select(int Id)
+        {
+            return DbContext.Treatments.SingleOrDefault(t => t.Id == Id);
+        }
+
+        public bool Update(Treatment t)
+        {
+            Treatment treatment = DbContext.Treatments.SingleOrDefault(x => x.Id == t.Id);
+            treatment = t;
+            return (DbContext.SaveChanges() > 0);
+        }
+    }
+}
