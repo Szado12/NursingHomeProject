829c57d0a936f418dd4bb00799b5f4fdf52b530e
Szado12 <51293021+Szado12@users.noreply.github.com>
2021-01-06 16:34:46 +0100

    Rehabilitant Form added Logical Manager
    Rehabilitant Form added Logical Manager

---
+++ NursingHomeApp/Systems/LogicalManagers/PlaceManager.cs
@@ -0,0 +1,43 @@
+﻿using NursingHomeApp.Systems.DataManagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NursingHomeApp.Systems.LogicalManagers
+{
+    class PlaceManager
+    {
+        PlaceDataManager placeDataManager = new PlaceDataManager();
+        public bool Add(string name)
+        {
+            Place place = new Place()
+            {
+                Name = name
+            };
+            return placeDataManager.Add(place);
+        }
+        public bool Delete(int Id)
+        {
+            return placeDataManager.Delete(Id);
+        }
+        public bool Update(int Id,string name)
+        {
+            Place place = new Place()
+            {
+                Id = Id,
+                Name = name
+            };
+            return placeDataManager.Update(place);
+        }
+        public List<Place> Select()
+        {
+            return placeDataManager.Select();
+        }
+        public Place Select(int Id)
+        {
+            return placeDataManager.Select(Id);
+        }
+    }
+}
